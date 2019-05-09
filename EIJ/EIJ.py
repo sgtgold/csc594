86003a1a07b9ca2ea868957fea488dd3d2cfc698
sgtgold <jacobp881@gmail.com>
2019-05-08 22:01:27 -0500

    First Save and Load Version
    Added in JSON save and load function

--- EIJ/EIJ.py
+++ EIJ/EIJ.py
@@ -1,8 +1,20 @@
 from pyknow import *
- 
+import json
+import os
+#Parameters
+workingDir = 'C:\\Users\\Jacob Penrod\\Google Drive\\DePaul\\CSC594\\EIJ\\'
+data = {}
+data['facts'] = []
 class GatherFacts(KnowledgeEngine):
     @DefFacts()
     def _initial_action(self):
+        with open(workingDir+'facts.json') as json_file:  
+            if os.stat(workingDir+'facts.json').st_size > 0:
+                data = json.load(json_file)
+                InitialFact()
+                for p in data['facts']:
+                    engine.declare(Fact(who=p['who'],what=p['what'],where=p['where'],why=p['why'],when=p['when']))                
+                print(engine.facts)
         yield Fact(action='gather')
     
     @Rule(Fact(action='gather'),
@@ -37,7 +49,26 @@ class GatherFacts(KnowledgeEngine):
         Fact(when=MATCH.when),
         Fact(why=MATCH.why))
     def gather(self,who,what,where,when,why):
-        print("Hi %s! How is the weather in %s?" % (who, where))
+        print("The Facts\nWho: %s\nWhat: %s\nWhen: %s\nWhere: %s\nWhy: %s" % (who,what,when,where,why))   
+        data['facts'].append({  
+        'who': who,
+        'what': what,
+        'where': where,
+        'when': when,
+        'why': why
+        })
+        print(engine.facts)
+        with open(workingDir+'facts.json', 'w') as outfile:  
+            json.dump(data, outfile)
+
+engine = KnowledgeEngine()
 engine = GatherFacts()
-engine.reset()  # Prepare the engine for the execution.
+engine.reset()
 engine.run()
+i=''
+while i != 'quit':
+    i = input('Would you like to enter in another event? (type quit to stop) ')
+    if i!= 'quit':
+        engine.reset()
+        engine.run()
+   # print('Here are the current facts',engine.facts)
